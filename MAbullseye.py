2734a01f831802e2f245bf8c2251470eb80fba4d
csPython <cspython@AD25TF04NGF1J-61.local>
2019-03-15 09:37:36 -0400

    First commit

--- MAbullseye.py
+++ MAbullseye.py
@@ -1,3 +1,9 @@
 from graphics import*
-def (Color,circlesz):
+def (Color,circlesz,x,y):
+    
+
+WindowSize = int(input("Enter number"))
+BulleyeWin = GraphWin("Bull eye",WindowSize,WindowSize)
+BulleyeWin.setCoords(0,0,WindowSize,WindowSize)
+    
     
